43cc98f8abac15d0e17d04d1bb30bbdee72121e0
Nick Farley <nick@nfreader.net>
2018-10-19 17:35:53 -0400

    Case sensitivity

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if(getenv('DEBUG')){
 
 // Instantiate the app
 $settings = require __DIR__ . '/src/settings.php';
-$settings['statbus'] = require __DIR__ . '/src/conf/statbus.php';
+$settings['statbus'] = require __DIR__ . '/src/conf/Statbus.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
