ca74a6ac9fe12cecff08e04b0bf852a5c911008a
Nick Farley <nick@nfreader.net>
2018-10-20 13:05:12 -0400

    AAAAAAAAa

--- public/index.php
+++ public/index.php
@@ -9,11 +9,11 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
-$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv = new Dotenv\Dotenv(__DIR__.'/..');
 $dotenv->load();
 
 if(getenv('DEBUG')){
@@ -23,21 +23,21 @@ if(getenv('DEBUG')){
 }
 
 // Instantiate the app
-$settings = require __DIR__ . '/src/settings.php';
-$settings['statbus'] = require __DIR__ . '/src/conf/Statbus.php';
+$settings = require __DIR__ . '/../src/settings.php';
+$settings['statbus'] = require __DIR__ . '/../src/conf/Statbus.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require __DIR__ . '/src/dependencies.php';
+require __DIR__ . '/../src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/src/middleware.php';
+require __DIR__ . '/../src/middleware.php';
 
 // Remove trailing slashes
-require __DIR__ . '/src/trailingSlash.php';
+require __DIR__ . '/../src/trailingSlash.php';
 
 // Register routes
-require __DIR__ . '/src/routes.php';
+require __DIR__ . '/../src/routes.php';
 
 // Run app
 $app->run();
