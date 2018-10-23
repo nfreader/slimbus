1b94d0306d498a30c914c5fe0b6e5114e52f374e
Nick Farley <nick@nfreader.net>
2018-10-23 12:51:19 -0400

    Better session handling
    Stop logging me out argh

--- public/index.php
+++ public/index.php
@@ -12,10 +12,41 @@ if (PHP_SAPI == 'cli-server') {
 
 require __DIR__ . '/../vendor/autoload.php';
 
-session_start();
-
 $dotenv = new Dotenv\Dotenv(__DIR__.'/..');
 $dotenv->load();
+
+if(php_sapi_name() != 'cli'){
+  if(!getenv('DEBUG')){
+    session_start([
+        'cookie_httponly' => true,
+        'cookie_secure' => true
+    ]);
+  } else {
+    session_start();
+  }
+  // Make sure we have a canary set
+  if (!isset($_SESSION['canary'])) {
+      session_regenerate_id(true);
+      $_SESSION['canary'] = time();
+  }
+  // Regenerate session ID every five minutes:
+  if ($_SESSION['canary'] < time() - 300) {
+      session_regenerate_id(true);
+      $_SESSION['canary'] = time();
+  }
+  
+  //Set session expiry to five days
+  $time = $_SERVER['REQUEST_TIME'];
+  $timeout_duration = 432000;
+  if (isset($_SESSION['LAST_ACTIVITY']) && 
+     ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+      session_unset();
+      session_destroy();
+      session_start();
+  }
+
+  $_SESSION['LAST_ACTIVITY'] = $time;
+}
 
 if(getenv('DEBUG')){
   ini_set('xdebug.var_display_max_depth',-1);
