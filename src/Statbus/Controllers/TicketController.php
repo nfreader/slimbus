c22010a59a6e197570733a20f8955c96f712f9b7
Nick <nick@nfreader.net>
2020-02-22 09:47:14 -0500

    Revert "Ticket query optimizations"
    This reverts commit f71bc1d685974a28aa3620f93ae10cd8bf7af488.

--- src/Statbus/Controllers/TicketController.php
+++ src/Statbus/Controllers/TicketController.php
@@ -5,13 +5,15 @@ namespace Statbus\Controllers;
 use Psr\Container\ContainerInterface;
 use Statbus\Controllers\Controller as Controller;
 use Statbus\Models\Ticket as Ticket;
-use Statbus\Controllers\UserController as User;
+use Statbus\Models\Player as Player;
+
 
 class TicketController extends Controller {
   public function __construct(ContainerInterface $container) {
     parent::__construct($container);
     $settings = $this->container->get('settings')['statbus'];
     $this->tm = new Ticket($settings);
+    $this->pm = new Player($settings);
     // $this->pages = ceil($this->DB->cell("SELECT count(tbl_messages.id) FROM tbl_messages WHERE tbl_messages.deleted = 0
     //   AND (tbl_messages.expire_timestamp > NOW() OR tbl_messages.expire_timestamp IS NULL)") / $this->per_page);
     $this->url = $this->router->pathFor('ticket.index');
@@ -29,27 +31,35 @@ class TicketController extends Controller {
         t.timestamp,
         t.recipient as recipient_ckey,
         t.sender as sender_ckey,
-        (SELECT `action` FROM tbl_ticket WHERE t.ticket = ticket AND t.round_id = round_id ORDER BY id DESC LIMIT 1) as `status`,
-        (SELECT COUNT(id) FROM tbl_ticket WHERE t.ticket = ticket AND t.round_id = round_id) as `replies`
-      FROM tbl_ticket t
+        r.rank as r_rank,
+        s.rank as s_rank,
+        COUNT(B.id) AS replies,
+        (SELECT `action` FROM ss13ticket WHERE t.ticket = ticket AND t.round_id = round_id ORDER BY id DESC LIMIT 1) as `status`
+      FROM ss13ticket t
+      LEFT JOIN ss13admin AS r ON r.ckey = t.recipient
+      LEFT JOIN ss13admin AS s ON s.ckey = t.sender
+      JOIN ss13ticket AS B ON B.ticket = t.ticket AND B.round_id = t.round_id
       WHERE t.action = 'Ticket Opened' 
       GROUP BY t.id
       ORDER BY `timestamp` DESC
       LIMIT ?, ?;", ($this->page * $this->per_page) - $this->per_page, $this->per_page);
     foreach ($tickets as &$t){
+      $t->sender = new \stdclass;
+      $t->sender->ckey = $t->sender_ckey;
+      $t->sender->rank = $t->s_rank;
+      $t->sender = $this->pm->parsePlayer($t->sender);
+
+      $t->recipient = new \stdclass;
+      $t->recipient->ckey = $t->recipient_ckey;
+      $t->recipient->rank = $t->r_rank;
+      $t->recipient = $this->pm->parsePlayer($t->recipient);
+
       $t = $this->tm->parseTicket($t);
     }
     return $tickets;
   }
 
-  public function getSingleTicket(int $round, int $ticket, $verifyPlayer = false){
-    $round = filter_var($round, FILTER_VALIDATE_INT);
-    $ticket = filter_var($ticket, FILTER_VALIDATE_INT);
-    $verify = null;
-    $verifyPlayer = filter_var('verifyPlayer', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-    if ($verifyPlayer) {
-      $verify = "AND (t.recipient = ? OR t.sender = ?)";
-    }
+  public function getSingleTicket(int $round, int $ticket){
     $tickets = $this->DB->run("
       SELECT
         t.id,
@@ -69,44 +79,21 @@ class TicketController extends Controller {
       LEFT JOIN tbl_admin AS s ON s.ckey = t.sender
       WHERE t.round_id = ?
       AND t.ticket = ? 
-      $verify
-      ORDER BY `timestamp` ASC;", $round, $ticket, $verifyPlayer, $verifyPlayer);
+      ORDER BY `timestamp` ASC;", $round, $ticket);
     foreach ($tickets as &$t){
+      $t->sender = new \stdclass;
+      $t->sender->ckey = $t->sender_ckey;
+      $t->sender->rank = $t->s_rank;
+      $t->sender = $this->pm->parsePlayer($t->sender);
+
+      $t->recipient = new \stdclass;
+      $t->recipient->ckey = $t->recipient_ckey;
+      $t->recipient->rank = $t->r_rank;
+      $t->recipient = $this->pm->parsePlayer($t->recipient);
+
       $t = $this->tm->parseTicket($t);
     }
     return $tickets;
-  }
-
-  public function getPlayerTickets($ckey) {
-    $ckey = filter_var($ckey, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-    $tickets = $this->DB->run("SELECT 
-      t.server_ip,
-      t.server_port as port,
-      t.round_id as round,
-      t.ticket,
-      t.action,
-      t.message,
-      t.timestamp,
-      t.recipient as recipient_ckey,
-      t.sender as sender_ckey,
-      r.rank as r_rank,
-      s.rank as s_rank,
-      COUNT(B.id) AS replies,
-      (SELECT `action` FROM tbl_ticket WHERE t.ticket = ticket AND t.round_id = round_id ORDER BY id DESC LIMIT 1) as `status`
-    FROM tbl_ticket t
-    LEFT JOIN tbl_admin AS r ON r.ckey = t.recipient
-    LEFT JOIN tbl_admin AS s ON s.ckey = t.sender
-    JOIN tbl_ticket AS B ON B.ticket = t.ticket AND B.round_id = t.round_id
-    WHERE (t.recipient = ? OR t.sender = ?) AND t.action = 'Ticket Opened'
-    GROUP BY t.id
-    ORDER BY `timestamp` DESC
-    LIMIT ?, ?;", $ckey, $ckey, ($this->page * $this->per_page) - $this->per_page, $this->per_page);
-    foreach ($tickets as &$t){
-      $t = $this->tm->parseTicket($t);
-    }
-
-    return $tickets;
-
   }
 
   public function index($request, $response, $args) {
@@ -122,27 +109,7 @@ class TicketController extends Controller {
         'ticket' => $this,
       ]);
   }
-
-
-  public function myTickets($request, $response, $args) {
-    if(isset($args['page'])) {
-      $this->page = filter_var($args['page'], FILTER_VALIDATE_INT);
-    }
-    $this->pages = ceil($this->DB->cell("SELECT
-      count(t.id) 
-      FROM tbl_ticket t
-      WHERE (t.recipient = ? OR t.sender = ?) AND t.action = 'Ticket Opened';", $ckey, $ckey) / $this->per_page);
-    $this->isPublic = TRUE;
-    return $this->view->render($this->response, 'me/tickets.tpl',[
-        'tickets' => $this->getPlayerTickets($this->container->user->ckey),
-        'ticket' => $this,
-      ]);
-  }
-
-  public function singlePlayerTicket($request, $response, $args){
-    $user = (new User($this->container))->fetchUser();
-    return $this->view->render($this->response, 'me/single_ticket.tpl',[
-        'tickets' => $this->getSingleTicket($args['round'],$args['ticket'],$user->ckey),
+        'wide' => TRUE
       ]);
   }
 
